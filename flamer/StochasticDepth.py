330badc57d082db7401bca900ace5e4b5fc47eb3
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-10 17:42:56 +0200

    wip

--- flamer/StochasticDepth.py
+++ flamer/StochasticDepth.py
@@ -4,18 +4,21 @@ import torch
 class StochasticDepth(torch.nn.Module):
     def __init__(self, probability: float):
         super().__init__()
-        assert probability <= 1.0 and probability >= 0, "0 <= prob <= 1"
+        assert probability <= 1.0 and probability >= 0
         self.keep_probability = 1 - probability
 
-    def __intact_condition(self) -> bool:
+    def __skip_condition(self) -> bool:
         return not self.training or self.keep_probability == 1.0
 
     def __get_mask(self, x: torch.Tensor) -> torch.Tensor:
         batch_size = x.size(0)
-        return torch.rand(batch_size, 1, 1, 1)
+        mask_size = (batch_size,) + (1,) * (x.ndim - 1)
+        return (torch.rand(mask_size, device=x.device) < self.keep_probability).to(
+            dtype=x.dtype
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.__intact_condition():
+        if self.__skip_condition():
             return x
 
         mask = self.__get_mask(x)
