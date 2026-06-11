0611676e5c849c6d5176e9aec090129c4d190b25
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-11 16:32:37 +0200

    adds gegelu

---
+++ flamer/gated_linear_units/GeGLU.py
@@ -0,0 +1,11 @@
+import torch
+from .GatedLinearUnit import GatedLinearUnit
+
+
+class GeGLU(GatedLinearUnit):
+    def __init__(self, input_dimension: int, output_dimension: int):
+        super().__init__(
+            input_dimension=input_dimension,
+            output_dimension=output_dimension,
+            activation=torch.nn.GELU,
+        )
