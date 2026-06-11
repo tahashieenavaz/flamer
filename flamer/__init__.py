f89e7f14d8501cd91d9c53903bbddcaf94e4e8b3
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-11 17:48:08 +0200

    init

--- flamer/__init__.py
+++ flamer/__init__.py
@@ -5,6 +5,8 @@ __version__ = version("baloot")
 from .reinforcement_learning import HLGaussLoss
 from .image import LayerNorm2d
 from .image import SqueezeExcitation
+from .image import PatchEmbedding
+from .image import PatchEmbedding as PatchEmbed
 from .loss import BalancedSoftmaxLoss
 from .loss import LearnableLogitNormLoss
 from .loss import LogitNormLoss
