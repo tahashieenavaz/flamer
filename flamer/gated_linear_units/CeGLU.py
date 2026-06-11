9ae78a62c277401333da8710e06422e8201a5774
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-11 16:50:06 +0200

    adds CeGLU

--- flamer/gated_linear_units/CeGLU.py
+++ flamer/gated_linear_units/CeGLU.py
@@ -2,10 +2,10 @@ import torch
 from .GatedLinearUnit import GatedLinearUnit
 
 
-class SeGLU(GatedLinearUnit):
+class CeGLU(GatedLinearUnit):
     def __init__(self, input_dimension: int, output_dimension: int):
         super().__init__(
             input_dimension=input_dimension,
             output_dimension=output_dimension,
-            activation=torch.nn.SELU,
+            activation=torch.nn.CELU,
         )
