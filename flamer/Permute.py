2fe00890f1d8515b0ab536f04f6125c3b08ae50b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-20 18:16:37 +0200

    adds permute module

---
+++ flamer/Permute.py
@@ -0,0 +1,10 @@
+import torch
+from typing import Tuple
+
+class Permute(torch.nn.Module):
+  def __init__(self, target_shape: Tuple[int]):
+    super().__init__()
+    self.target_shape = target_shape
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    return x.permute(self.target_shape)
