339b9c889112778a8db96cbfd1353aea091e4fe8
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-20 18:20:59 +0200

    fixes reshape and permute usage

--- flamer/Permute.py
+++ flamer/Permute.py
@@ -2,7 +2,7 @@ import torch
 from typing import Tuple
 
 class Permute(torch.nn.Module):
-  def __init__(self, target_shape: Tuple[int]):
+  def __init__(self, *target_shape: Tuple[int]):
     super().__init__()
     self.target_shape = target_shape
 
