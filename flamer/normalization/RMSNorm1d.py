73d73469ce2b4728675b66de7c24861e4d6cb3b2
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-20 18:31:03 +0200

    adds RMSNorm1d

---
+++ flamer/normalization/RMSNorm1d.py
@@ -0,0 +1,11 @@
+import torch
+
+class RMSNorm1d(torch.nn.Module):
+    def __init__(self, channels: int, epsilon: float = 1e-8):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.ones(1, channels, 1))
+        self.epsilon = epsilon
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        rms = torch.sqrt(torch.mean(x**2, dim=1, keepdim=True) + self.epsilon)
+        return (x / rms) * self.weight
