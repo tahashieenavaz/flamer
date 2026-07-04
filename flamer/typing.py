0b8dd13fbbc9c1ba174aea9808325439bb7ddbfc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-04 13:48:15 +0200

    enhances normalization layer type

--- flamer/typing.py
+++ flamer/typing.py
@@ -2,5 +2,9 @@ import torch
 from typing import TypeAlias, Union
 
 NormalizationLayer: TypeAlias = Union[
-    torch.nn.LayerNorm, torch.nn.RMSNorm, torch.nn.BatchNorm
+    torch.nn.LayerNorm,
+    torch.nn.RMSNorm,
+    torch.nn.BatchNorm2d,
+    torch.nn.BatchNorm1d,
+    torch.nn.BatchNorm3d,
 ]
