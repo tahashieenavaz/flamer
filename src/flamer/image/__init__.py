56476287c8e9d863d14ec6a0ad2005b70cf2aa72
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-19 12:30:58 +0200

    fix: renames image modules' files

--- src/flamer/image/__init__.py
+++ src/flamer/image/__init__.py
@@ -1,3 +1,3 @@
-from .SqueezeExcitation import SqueezeExcitation
-from .LayerNorm2d import LayerNorm2d
-from .PatchEmbedding import PatchEmbedding
+from .squeeze_excitation import SqueezeExcitation
+from .layer_norm_2d import LayerNorm2d
+from .patch_embedding import PatchEmbedding
